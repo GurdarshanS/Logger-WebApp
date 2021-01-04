096ebebcaea780fc4ca25229fefe5d40c6f0b0af
GurdarshanS <59708264+GurdarshanS@users.noreply.github.com>
2021-01-03 20:45:38 -0500

    Final Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+
+function Header() {
+  return (
+    <header>
+      <h1> <LibraryBooksIcon /> Logger</h1>
+    </header>
+  );
+}
+
+export default Header;
